aa417bba7e7f47d969d00175b57077f00c3b7ad4
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2023-12-28 11:32:11 -0300

    Add borders to preview image

--- build_all.sh
+++ build_all.sh
@@ -27,7 +27,7 @@ cd ..
 mv stage2/* ./cursors
 
 # create montage
-montage -geometry +0+0 -tile 5x ./stage1/{arrow0,left_ptr_watch0,grab0,watch0,X_cursor0,right_side0,based_arrow_down0,bottom_left_corner0,bottom_right_corner0,center_ptr0,cross0,fleur0,question_arrow0,xterm0}.png montage.png
+montage -geometry +0+0 -tile 5x ./stage1/{arrow0,left_ptr_watch0,grab0,watch0,X_cursor0,right_side0,based_arrow_down0,bottom_left_corner0,bottom_right_corner0,center_ptr0,cross0,fleur0,question_arrow0,xterm0}.png -bordercolor white -border 10x10 montage.png
 
 
 rm -r stage{0,1,2}
