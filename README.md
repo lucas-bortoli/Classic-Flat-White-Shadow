830f9beaa7db61f58c83fed5b0a56480acbc1db6
Lucas Vinicius de Bortoli Santos <lucasbortolisantos@gmail.com>
2023-12-28 11:16:01 -0300

    Create repository and build files

---
+++ README.md
@@ -0,0 +1,7 @@
+# Classic-Flat-White Shadow
+
+![Preview of the cursors](./montage.png)
+
+A fork of the excellent cursor theme **[Classic Flat White](https://www.gnome-look.org/p/1102959/)** adding Windows-like shadows.
+
+The shadow parameters may be changed in the `build_all.sh` file (follow [ImageMagick's shadow documentation](https://legacy.imagemagick.org/Usage/blur/#shadow) for reference). The build file requires imagemagick and xcursorgen.
---
+++ build_all.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+rm -r stage{0,1,2} || true
+mkdir -p stage{0,1,2}
+
+# add shadow
+cp Sources/*.png stage0
+cd stage0
+for i in *.png; do 
+  echo "adding shadow: $i"
+  convert "$i" \( +clone -background black -shadow 30x1+2+2 \) +swap -background none -layers merge +repage "../stage1/$i"
+done
+cd ..
+
+# create x cursors
+cp Sources/*.conf stage1
+cd stage1
+for f in *.conf; do
+  CURSORNAME="$(echo $f | sed "s/.conf//")"
+  echo "creating cursor: $f -> $CURSORNAME"
+  xcursorgen "$f" "../stage2/$CURSORNAME"
+done
+
+cd ..
+mv stage2/* ./cursors
+
+# create montage
+montage -geometry +0+0 -tile 5x ./stage1/{arrow0,left_ptr_watch0,grab0,watch0,X_cursor0,right_side0,based_arrow_down0,bottom_left_corner0,bottom_right_corner0,center_ptr0,cross0,fleur0,question_arrow0,xterm0}.png montage.png
+
+
+rm -r stage{0,1,2}
+
